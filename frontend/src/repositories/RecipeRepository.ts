acf94d73917173de8c24e3b35b6dc3fffc72b948
dando90 <98711470+dando90@users.noreply.github.com>
2024-06-17 17:28:02 +0200

    fix: added forPage value in env variables

--- frontend/src/repositories/RecipeRepository.ts
+++ frontend/src/repositories/RecipeRepository.ts
@@ -3,6 +3,7 @@ import { RecipeAPI } from "../types/recipe";
 
 class RecipeRepository extends BaseRepository<RecipeAPI> {
   collection = "recipes";
+  forPage = import.meta.env.VITE_RECIPES_FOR_PAGE || 5;
 }
 
 export default RecipeRepository;
